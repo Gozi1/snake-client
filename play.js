f50c0ef524c8de0766d8f1084a0935797389ee93
Gozi1 <snwabiani@gmail.com>
2022-10-20 02:40:33 +0000

    change made to play and adding client

--- play.js
+++ play.js
@@ -1,22 +1,6 @@
 const net = require("net");
 
 // establishes a connection with the game server
-const connect = function () {
-  const conn = net.createConnection({
-    host: "10.0.2.15",
-    port: 50541,
-  });
+const {connect} = require('./client'); 
 
-  // interpret incoming data as text
-  conn.setEncoding("utf8");
-
-  conn.on("data", (data) => {
-    console.log(data);
-  });
-
-  return conn;
-};
-
-console.log("Connecting ...");
 connect();
-
