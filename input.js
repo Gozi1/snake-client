099c877356383781df304bfa3c32c15aa0313bcc
Gozi1 <snwabiani@gmail.com>
2022-10-31 00:28:27 +0000

    moving snake

--- input.js
+++ input.js
@@ -1,37 +1,53 @@
-const setupInput = function () {
+const { Console } = require("console");
+
+let connection;
+// stdin setup
+const setupInput = (conn) => {
+  // the conn object
+  connection = conn;
   const stdin = process.stdin;
   stdin.setRawMode(true);
   stdin.setEncoding("utf8");
   stdin.resume();
   return stdin;
 };
+
 const handleUserInput = function (key) {
-  // your code here
+  
+  // exits when Ctrl + c are pressed
   if ( key === '\u0003' ) {
     process.exit();
   }
-
-  if(key === ""){
-    conn.on("connect", () => {
-        conn.write("Move: up");
+  //prints out key pressed
+  console.log(key);
+  //moves snake up
+  if(key === "w"){
+    connection.on("connect", () => {
+        connection.write("Move: up");
+    });
+  }
+  //moves snake down
+  if(key === "s"){
+    connection.on("connect", () => {
+        connection.write("Move: down");  
       });
   }
-  if(key === ""){
-    conn.on("connect", () => {
-        conn.write("Move: down");
+  //moves snake left
+  if(key === "a"){
+    connection.on("connect", () => {
+        connection.write("Move: left");
       });
+      
   }
-  if(key === ""){
-    conn.on("connect", () => {
-        conn.write("Move: left");
+  //moves snake right
+  if(key === "d"){
+    connection.on("connect", () => {
+        connection.write("Move: right");
       });
-  }
-  if(key === ""){
-    conn.on("connect", () => {
-        conn.write("Move: right");
-      });
+      
   }
 };
 setupInput().on("data",handleUserInput);
 
+//exporting setup input;
 module.exports = {setupInput};
--- play.js
+++ play.js
@@ -5,6 +5,5 @@ const { connect } = require("./client");
 const { setupInput } = require("./input");
 
 console.log("Connecting ...");
-connect();
 
-setupInput();
+setupInput(connect());
